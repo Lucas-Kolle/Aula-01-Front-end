5bfed97b8de2e11015298a1f093c199c82b7e666
LUCAS DIAS BRANDAO KOLLE <25203704@CFP127ALUNO.LOCAL>
2026-02-02 11:30:24 -0300

    finalizando prjeto

--- main.js
+++ main.js
@@ -3,8 +3,23 @@
 const botaoTrocarCor = document.getElementById("trocar-cor")
 
 function trocarCor (){
-    const cor = document.getElementById("cor").value 
+
+    let cor = document.getElementById("cor").value 
     document.documentElement.style.setProperty("--cor-bg", cor)
+    document.documentElement.style.setProperty("--cor-bg", cor)
+
+    if(cor = "azul"){
+        cor = "blue"
+        document.documentElement.style.setProperty("--cor-bg", cor)
+    }else if (cor = "preto"){
+        cor = "black"
+    }else if (cor = "laranja"){
+        cor = "orange"
+    }else if(cor = "branco"){
+        cor = "white"
+    }else{
+        document.documentElement.style.setProperty("--cor-bg", cor)
+    }
 }
 
 botaoTrocarCor.addEventListener("click", trocarCor)
