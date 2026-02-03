04c0cf1d4236585f37e5de0929e7a55626a8ed03
LUCAS DIAS BRANDAO KOLLE <25203704@CFP127ALUNO.LOCAL>
2026-02-03 07:51:56 -0300

    corigindo erros

--- main.js
+++ main.js
@@ -7,19 +7,6 @@ function trocarCor (){
     let cor = document.getElementById("cor").value 
     document.documentElement.style.setProperty("--cor-bg", cor)
     document.documentElement.style.setProperty("--cor-bg", cor)
-
-    if(cor = "azul"){
-        cor = "blue"
-        document.documentElement.style.setProperty("--cor-bg", cor)
-    }else if (cor = "preto"){
-        cor = "black"
-    }else if (cor = "laranja"){
-        cor = "orange"
-    }else if(cor = "branco"){
-        cor = "white"
-    }else{
-        document.documentElement.style.setProperty("--cor-bg", cor)
-    }
 }
 
 botaoTrocarCor.addEventListener("click", trocarCor)
